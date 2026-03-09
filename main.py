ccda5583a8c92ba7b2217b60b3aed733e9429efc
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-03-08 22:05:43 -0500

    feat: update main.py

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+from datetime import datetime
 def get_sign(date_str):
+    # declare and init a Zodiac Dictionary
     zodiac_dict = {
         "Aries" : ('03-21','04-19'),
-        "Taurus" : ('04-20', '05-20')
+        "Taurus" : ('04-20', '05-20'),
+        "Gemini" : ('05-21','06-20'),
+        "Cancer" : ('06-21', '07-22'),
+        "Leo" : ('07-23','08-22'),
+        "Virgo" : ('08-23', '09-22'),
+        "Libra" : ('09-23','10-22'),
+        "Scorpio" : ('10-23', '11-21'),
+        "Sagittarius" : ('11-22','12-21'),
+        "Capricorn" : ('12-22', '01-19'),
+        "Aquarius" : ('01-20','02-18'),
+        "Pisces" : ('02-19', '03-20')
     }
+    # define the format of date
+    date_format = "%Y-%m-%d"
+    # convert the date to a date object
+    date_obj = datetime.strptime(date_str, date_format)
+    # iterate over the zodiac to determine the sign
+    for key,value in zodiac_dict.items():
+        # Check if the sign is in an especific range
+        if datetime.strptime((date_str[:5]+value[0]), date_format) <= date_obj <= datetime.strptime((date_str[:5]+value[1]), date_format):return key
+        # Check Capricorn special sign because of the change of year
+        if key == 'Capricorn':
+            if datetime.strptime((date_str[:5]+value[0]), date_format) <= date_obj <= datetime.strptime((str(int(date_str[:4])+1)+'-'+value[1]), date_format):return key
 
-    return date_str
 
 if __name__ == '__main__':
     print(get_sign("2026-01-31"))
